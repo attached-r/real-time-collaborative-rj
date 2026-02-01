9c166af2998f07df11e7062fcf16ecbf06bd1f5f
attached <2165329383@qq.com>
2026-02-01 17:11:17 +0800

    Initial commit: Spring Boot 项目通过 Initializr 生成，包含 Web/Security/WebSocket/MongoDB 依赖

---
+++ real-time-collaborative-doc/src/main/java/rj/collaborative/RealTimeCollaborativeDocApplication.java
@@ -0,0 +1,13 @@
+package rj.collaborative;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealTimeCollaborativeDocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RealTimeCollaborativeDocApplication.class, args);
+    }
+
+}
---
+++ real-time-collaborative-doc/src/test/java/rj/collaborative/RealTimeCollaborativeDocApplicationTests.java
@@ -0,0 +1,13 @@
+package rj.collaborative;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealTimeCollaborativeDocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
